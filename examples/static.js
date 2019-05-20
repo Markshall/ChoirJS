8058e6786b88b09d7e2f4feadae43109130c58a8
jtPox <me@jtpox.com>
2019-05-20 21:51:22 +0800

    Added comment in example file.

--- examples/static.js
+++ examples/static.js
@@ -3,6 +3,9 @@ import Choir from '../index';
 const port = 8080;
 const choir = new Choir(port);
 
+/*
+ * Serve static files in a directory. It is recursive.
+ */
 choir.files('directory/');
 
 choir.core.on('start', () => {
